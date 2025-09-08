a2ddb229bfed36b71a42ae80b40247debe96eb3d
Souvik-Vikie <souvikkarmakar368@gmail.com>
2025-09-09 03:19:57 +0530

    Running fine

--- frontend/src/api.ts
+++ frontend/src/api.ts
@@ -1,8 +1,8 @@
 // src/api.ts
-// export const API_BASE_URL =
-//   process.env.REACT_APP_API_BASE || "http://localhost:8000";
+export const API_BASE_URL =
+  process.env.REACT_APP_API_BASE || "http://localhost:8000";
 
-const API_BASE_URL = "https://medgen-souvik-backend.onrender.com";
+// const API_BASE_URL = "https://medgen-souvik-backend.onrender.com";
 
 /**
  * Upload an image to the backend /predict endpoint.
